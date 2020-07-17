682a356854d9124dbcc49f0b36aea3e4ee73b49d
lioil9 <156949416@qq.com>
2020-07-17 17:31:38 +0800

    update

---
+++ Java-Web/work_7_17/src/main/java/club/banyuan/controller/HelloController.java
@@ -0,0 +1,14 @@
+package club.banyuan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+  @RequestMapping("hello")
+  public String hello(){
+    System.out.println("controller---hello");
+    return "hello";
+  }
+}
---
+++ Java-Web/work_7_17/src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,7 @@
+<%@page contentType="text/html; charset=UTF-8" language="java" %>
+<html>
+<body>
+<h2><a href="hello">Hello World!</a></h2>
+<img style="width: 300px; height: 200px" src="images/22.jpg"/>
+</body>
+</html>
